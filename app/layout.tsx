7cffcf47b9a6964702f0b36e6f3afc0cb3f21788
unknown <ramirocapdevila23@gmail.com>
2026-03-25 16:07:52 -0300

    fix provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
+import RecaptchaProvider from "@/components/RecaptchaProvider"; // ✅
 
 const urbanist = Urbanist({
   subsets: ["latin"],
@@ -21,9 +21,9 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={`${urbanist.variable} antialiased`}>
-        <GoogleReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}>
+        <RecaptchaProvider>
           {children}
-        </GoogleReCaptchaProvider>
+        </RecaptchaProvider>
       </body>
     </html>
   );
